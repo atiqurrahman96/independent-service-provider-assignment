01ad5eff2432975e35289f66e0714c9bfd5b4669
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-04-18 01:24:35 +0600

    custom link hasbeen added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -4,6 +4,7 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
+import CustomLink from '../../Pages/Home/CustomLink/CustomLink';
 
 const Header = () => {
     const [user] = useAuthState(auth);
@@ -21,15 +22,15 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
-                            <Nav.Link as={Link} to="/about">About</Nav.Link>
-                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/home">Home</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/blogs">Blogs</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/about">About</Nav.Link>
 
                         </Nav>
                         <Nav>
                             {user ? <button className='bg-success rounded-pill text-white' onClick={handleSignOut}>Sign Out</button> :
-                                <Nav.Link as={Link} to="/logIn">Log In</Nav.Link>}
-                            <Nav.Link as={Link} to="/signUp" eventKey={2} href="#memes">
+                                <Nav.Link as={CustomLink} to="/logIn">Log In</Nav.Link>}
+                            <Nav.Link as={CustomLink} to="/signUp" eventKey={2} href="#memes">
                                 Sign Up
                             </Nav.Link>
                         </Nav>
